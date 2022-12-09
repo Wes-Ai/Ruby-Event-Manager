4f972e8871e76da35c35dfcfe19942a8f8f6e2cd
Wesley Smith <smithw26@wwu.edu>
2022-12-09 09:10:44 -0800

    Added phone number cleaning functionality.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,10 +1,26 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
-
-template_letter = File.read('form_letter.html')
+require 'erb'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, '0')[0..4]
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def clean_phone_numbers(num)
+  num.gsub!(/[^0-9]/, '')
+  # Replace bad phone numbers with this
+  bad_num = '0000000000'
+  len = num.length
+
+  case len
+  when len = 10 then format_phone_number(num)
+  when (len = 11 && num[0] == 1) then format_phone_number(num[0,9])
+  else format_phone_number(bad_num)
+  end
+end
+
+def format_phone_number(num)
+  '(' + num[0,3] + ') ' + num[3,3] + '-' + num[6,4]
 end
 
 def legislators_by_zipcode(zip)
@@ -12,16 +28,23 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(", ")
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
   end
 end
 
@@ -33,12 +56,17 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
+  p phone_num = clean_phone_numbers(row[:homephone])
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id,form_letter)
 end
